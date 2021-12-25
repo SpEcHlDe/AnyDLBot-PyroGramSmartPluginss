8c84035741ca0dd1cf8f81e16fad98b20ac98ae9
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-25 03:37:52 -0600

    Initial commit

---
+++ translation.py
@@ -0,0 +1,33 @@
+class Translation(object):
+    START_TEXT = "/help to know how to use me\n/upgrade for Premium Features\nSource Code: https://github.com/SpEcHiDe/AnyDLBot \n© @AnyDLBot"
+    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
+    ABS_TEXT = " Please don't be selfish."
+    UPGRADE_TEXT = "@AnyDLBot <a href='https://t.me/SpEcHlDe/599'>Paid Plans</a>\nMake your payment and send screenshot to @shrimadhabot OR @SpEcHIDe"
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> "
+    NOYES_URL = "@GetPublicLinkBot URL detected. Please do not abuse the service!"
+    DOWNLOAD_START = "trying to download"
+    UPLOAD_START = "trying to upload"
+    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+    RCHD_TG_API_LIMIT = "Sorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. https://t.me/storebot?start=AnyDLBot"
+    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video thumbnail saved. This image will be used in the above video."
+    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+    CUSTOM_CAPTION_UL_FILE = " © @AnyDLBot"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom THumbNail found. Please goto @AnyDL_Bot"
+    NO_VOID_FORMAT_FOUND = "Can you check the url? I am unable to detect video format from URL. If you think this could be a bug please report on https://github.com/spechide/AnyDLBot/issues"
+    HELP_USER = """There are multiple things I can do:
+👉 All Supported Video Formats of https://rg3.github.io/youtube-dl/supportedsites.html
+👉 Upload as file from any HTTP link
+👉 Convert To Streamable Video, any Telegram media
+👉 ReName Telegram files, with custom thumbnail support
+👉 Get High Speed Direct Download Link of any Telegram file
+--------"""
+    REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
+    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to /converttovideo"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support"
+    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\n© @AnyDLBot"
+    FF_MPEG_RO_BOT_RE_SURRECT_ED = """Reply to /getlink reply, with a timestamp (seperated by :) or a range of timestamps (seperated by ===)
+
+TimeStamp should be in the format HH:MM:SS"""
+    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Reply to a HTTP link, to extract embedded subtitle"
